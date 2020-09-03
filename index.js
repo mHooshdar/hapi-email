c11a74787880acc4c07faad683e5c356127d0d1a
Mohammad Hooshdar <mohammad.hooshdar@yahoo.com>
2020-09-04 00:47:55 +0430

    chore: add send email api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Hapi = require('@hapi/hapi');
 const imaps = require('imap-simple');
 const simpleParser = require('mailparser').simpleParser;
 const _find = require('lodash/find');
+const nodemailer = require('nodemailer');
 
 const config = {
   imap: {
@@ -14,6 +15,17 @@ const config = {
     tls: true,
   },
 };
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+  host: 'webmail.aut.ac.ir',
+  port: 25,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: 'test', // generated ethereal user
+    pass: 'password', // generated ethereal password
+  },
+});
 
 const init = async () => {
   const server = Hapi.server({
@@ -33,16 +45,36 @@ const init = async () => {
           bodies: ['HEADER', 'TEXT', ''],
         };
         const messages = await connection.search(searchCriteria, fetchOptions);
-        return Promise.all(messages.map(async (item) => {
-          const all = _find(item.parts, { which: '' });
-          var id = item.attributes.uid;
-          var idHeader = 'Imap-Id: ' + id + '\r\n';
-          const mail = await simpleParser(idHeader + all.body);
-          return mail
-        }));
+        return Promise.all(
+          messages.map(async (item) => {
+            const all = _find(item.parts, { which: '' });
+            var id = item.attributes.uid;
+            var idHeader = 'Imap-Id: ' + id + '\r\n';
+            const mail = await simpleParser(idHeader + all.body);
+            return mail;
+          }),
+        );
       } catch (e) {
         return e;
       }
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/send',
+    handler: async (request, h) => {
+      let info = await transporter.sendMail({
+        from: '"Fred Foo 👻" <m.hooshdar@aut.ac.ir>', // sender address
+        to: 'mohammad.hooshdar@yahoo.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>', // html body
+      });
+      return {
+        ...info,
+        msgUrl: nodemailer.getTestMessageUrl(info),
+      };
     },
   });
 
